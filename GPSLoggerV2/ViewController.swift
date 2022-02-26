31b70616250aa733834152d04f48d1d6876a4c50
k15015kk <aitk15015kk@outlook.com>
2022-02-26 14:19:11 +0900

    Initial Commit

---
+++ GPSLoggerV2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GPSLoggerV2
+//
+//  Created by 井上晴稀 on 2022/02/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
