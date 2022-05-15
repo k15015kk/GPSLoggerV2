2a970686807a3fa123d95cc29f883be9eded2165
k15015kk <aitk15015kk@outlook.com>
2022-05-16 00:13:49 +0900

    feature: 距離フィルタを設定する画面を実装

---
+++ GPSLoggerV2/ViewController/Setting/DistanceFilterSettingViewController.swift
@@ -0,0 +1,153 @@
+//
+//  DistanceFilterSettingController.swift
+//  GPSLoggerV2
+//
+//  Created by 井上晴稀 on 2022/05/15.
+//
+
+import UIKit
+
+class DistanceFilterSettingViewController: UITableViewController, UITextFieldDelegate, UIGestureRecognizerDelegate {
+    
+    @IBOutlet private weak var distanceFilterTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        distanceFilterTextField.delegate = self
+        
+        // UserDefaultが保存されている場合，それを表示する
+        if let distanceFilter = UserDefaults.standard.value(forKey: "distanceFilter") as? String {
+            distanceFilterTextField.text = distanceFilter
+        }
+        
+        // UITapGestureRecognizerを定義
+        let tapGesture:UITapGestureRecognizer = UITapGestureRecognizer(
+            target: self,
+            action: #selector(self.tapped(_:)))
+        
+        // デリゲートをセット
+        tapGesture.delegate = self
+        
+        self.view.addGestureRecognizer(tapGesture)
+        
+    }
+    
+    // MARK: TableViewのデータソース
+    
+    /// TableView内のセクション数を返します
+    /// - Parameter tableView: TableViewのオブジェクト
+    /// - Returns: セクション数
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    /// セクションごとのRow数を返します
+    /// - Parameters:
+    ///   - tableView: TableViewのオブジェクト
+    ///   - section: セクション情報（数値）
+    /// - Returns: セクションごとのRow数
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        /// セクションごとのRowを返す
+        switch section {
+        case 0:
+            return 1
+        default :
+            return 0
+        }
+    }
+    
+    // MARK: TextFieldDelegate
+    
+    /// キーボード内のエンターが呼ばれたときの処理
+    /// - Parameter textField: テキストフィールドの情報
+    /// - Returns: 終了値
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // キーボードを閉じる
+        self.distanceFilterTextField.endEditing(true)
+        return true
+    }
+    
+    /// キーボードが閉じる直前の処理
+    /// - Parameter textField: テキストフィールドの情報
+    /// - Returns: 終了値
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        
+        // 入力値が小数かどうかチェック
+        if checkFloatDistanceFilter(distance: textField.text) {
+
+            // 入力値が小数の場合，値をUserDefaultに保存
+            saveDistanceFilter(distance: textField.text)
+            
+        }
+        
+        return true
+    }
+    
+    // MARK: UITapGestureRecognizer
+    
+    /// 画面内をタップしたときの処理
+    /// - Parameter sender: sender
+    @objc func tapped(_ sender: UITapGestureRecognizer){
+        if sender.state == .ended {
+            self.view.endEditing(true)
+        }
+    }
+}
+
+extension DistanceFilterSettingViewController {
+    func checkFloatDistanceFilter(distance distanceFilter: String?) -> Bool {
+        
+        guard let distanceFilter = distanceFilter else {
+            return false
+        }
+        
+        if let distance = Float(distanceFilter){
+            
+            if distance < 0 {
+                
+                // 警告アラートを出す
+                presentWarningAlert("入力値不正", "0以上の小数を入力してください")
+                return false
+                
+            }
+            
+            return true
+        } else {
+            // 警告アラートを出す
+            presentWarningAlert("入力値不正", "小数を入力してください")
+            return false
+        }
+        
+    }
+    
+    func presentWarningAlert(_ title: String, _ message: String) {
+        // アラートを定義
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        // OKを押した際の処理を追加
+        let ok = UIAlertAction(title: "OK", style: .default) { (action) in
+            self.dismiss(animated: true, completion: nil)
+        }
+        
+        // アラートにOKボタンを追加
+        alert.addAction(ok)
+        
+        // アラートを表示
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func saveDistanceFilter(distance distanceFilter: String?) {
+        
+        guard let distanceFilter = distanceFilter else {
+            return
+        }
+        
+        let defaults = UserDefaults.standard
+        
+        if let distanceFilterValue = Float(distanceFilter) {
+            defaults.set(String(distanceFilterValue), forKey: "distanceFilter")
+            distanceFilterTextField.text = String(distanceFilterValue)
+        }
+        
+    }
+}
---
+++ GPSLoggerV2/ViewController/Setting/SettingViewController.swift
@@ -0,0 +1,63 @@
+//
+//  SettingViewController.swift
+//  GPSLoggerV2
+//
+//  Created by 井上晴稀 on 2022/05/15.
+//
+
+import UIKit
+
+class SettingViewController: UITableViewController {
+    
+    @IBOutlet private weak var distanceFilterLabel: UILabel!
+    @IBOutlet private weak var desiredAccuracyLabel: UILabel!
+    @IBOutlet private weak var activityTypeLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // UserDefaultが保存されている場合，それを表示する
+        if let distanceFilter = UserDefaults.standard.value(forKey: "distanceFilter") as? String {
+            
+            distanceFilterLabel.text = distanceFilter
+        }
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(userDefaultsDidChange),
+            name: UserDefaults.didChangeNotification,
+            object: nil
+        )
+    }
+    
+    // MARK: TableViewのデータソース
+    
+    /// TableView内のセクション数を返します
+    /// - Parameter tableView: TableViewのオブジェクト
+    /// - Returns: セクション数
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    /// セクションごとのRow数を返します
+    /// - Parameters:
+    ///   - tableView: TableViewのオブジェクト
+    ///   - section: セクション情報（数値）
+    /// - Returns: セクションごとのRow数
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        /// セクションごとのRowを返す
+        switch section {
+        case 0:
+            return 3
+        default :
+            return 0
+        }
+    }
+    
+    @objc func userDefaultsDidChange(_ notification: Notification) {
+        // UserDefaultsの変更があったので画面の情報を更新する
+        if let distanceFilter = UserDefaults.standard.value(forKey: "distanceFilter") as? String {
+            distanceFilterLabel.text = distanceFilter
+        }
+    }
+}
