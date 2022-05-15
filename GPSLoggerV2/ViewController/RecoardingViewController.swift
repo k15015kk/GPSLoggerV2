6559b7a6ae1413ec16bc8c72ba075ab06ab5fcb8
k15015kk <aitk15015kk@outlook.com>
2022-05-15 16:04:54 +0900

    first commit

--- GPSLoggerV2/ViewController/RecoardingViewController.swift
+++ GPSLoggerV2/ViewController/RecoardingViewController.swift
@@ -1,19 +1,17 @@
 //
-//  ViewController.swift
+//  RecoardingViewController.swift
 //  GPSLoggerV2
 //
-//  Created by 井上晴稀 on 2022/02/26.
+//  Created by 井上晴稀 on 2022/05/15.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class RecordingViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
 }
-
